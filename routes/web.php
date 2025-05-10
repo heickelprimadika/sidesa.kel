f0fa9fc5e02c64fe3ae615146a16d14a569f3004
Rafliyahyaprajaputra <raflibaturaja0@gmail.com>
2025-05-10 10:43:54 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResidentController;
+use App\Models\Resident;
+use Illuminate\Support\Facades\Route;
+
+//Auth
+Route::get('/', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'authenticate']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/register', [AuthController::class, 'registerView']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+});
+
+
+Route::get('/resident', [ResidentController::class, 'index']);
+Route::get('/resident/create', [ResidentController::class, 'create']);
+Route::get('/resident/{id}', [ResidentController::class, 'edit']);
+Route::post('/resident', [ResidentController::class, 'store']);
+Route::put('/resident/{id}', [ResidentController::class, 'update']);
+Route::delete('/resident/{id}', [ResidentController::class, 'destroy']);
